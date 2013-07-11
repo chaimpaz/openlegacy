ff881c5792410c759418fdb730aa382775757105
dganit <dganit.david@gmail.com>
2013-07-11 09:14:32 +0300

    #282: Integrate/write a cobol parser Support order of fields/parts

--- openlegacy-api/src/main/java/org/openlegacy/rpc/definitions/RpcPartEntityDefinition.java
+++ openlegacy-api/src/main/java/org/openlegacy/rpc/definitions/RpcPartEntityDefinition.java
@@ -3,6 +3,9 @@ package org.openlegacy.rpc.definitions;
 import org.openlegacy.definitions.PartEntityDefinition;
 
 public interface RpcPartEntityDefinition extends PartEntityDefinition<RpcFieldDefinition> {
+
 	public String getOriginalName();
+
+	public int getOrder();
 
 }
